fb7326193176bb727407ece4097295b760c002e0
Gilles Polart-Donat <gpolart@lutraconseil.fr>
2016-08-17 14:43:32 +0200

    Add watchdog

--- notifier.js
+++ notifier.js
@@ -9,8 +9,31 @@ var config = require('./config');
 syslog.init("notifier", syslog.LOG_NDELAY, syslog.LOG_LOCAL0);
 var transport = nodemailer.createTransport(config.smtp);
 
+var last_watch = new Date(); // last occurence of watchdog message
+// ---------------------------------------------------------------------
+// send_mail
+// ---------------------------------------------------------------------
+function send_mail(subject, text, cb) {
+    var options = { from : config.from,
+                  to : config.to,
+                  subject: subject,
+                  text: text
+                };
+    transport.sendMail(options, function(err, info) {
+        if (err) {
+          syslog.log(syslog.LOG_ERROR, "notifier error " + err);
+          cb("KO");
+        }
+        else {
+          syslog.log(syslog.LOG_INFO, "notifier send mail " + info.response);
+          cb('OK');
+        }
+    });
+}
+// ---------------------------------------------------------------------
+// HTTP server
+// ---------------------------------------------------------------------
 var server = http.createServer(function(req, res) {
-
   var body = [];
   req.on('error', function(err) {
     syslog.log(syslog.LOG_ERROR, "request error " + err);
@@ -18,37 +41,44 @@ var server = http.createServer(function(req, res) {
     body.push(chunk);
   }).on('end', function() {
     body = Buffer.concat(body).toString();
-
-    var options = { from : config.from,
-                  to : config.to,
-                  subject: "notifier",
-                  text: "No text"
-                };
+    var subject = "No subject";
+    var text = "No text";
 
     var data = qstring.parse(body);
-    if (data.hasOwnProperty('subject')) {
-        options.subject = data.subject;
-    }
-    if (data.hasOwnProperty('text')) {
-        options.text = data.text;
-    }
 
-    transport.sendMail(options, function(err, info) {
-      if (err) {
-          syslog.log(syslog.LOG_ERROR, "notifier error " + err);
+    if (data.subject === 'watchdog') {
+        last_watch = new Date();
+        res.writeHead(200);
+        res.end('OK');
+    }
+    else {
+        if (data.hasOwnProperty('subject')) {
+            subject = data.subject;
+        }
+        if (data.hasOwnProperty('text')) {
+            text = data.text;
+        }
+        send_mail(subject, text, function(msg) {
           res.writeHead(200);
-          res.end('KO');
-      }
-      else {
-          syslog.log(syslog.LOG_INFO, "notifier send mail " + info.response);
-          res.writeHead(200);
-          res.end('OK');
-      }
-    });
-
+          res.end(msg);
+        });
+    }
   });
-
 });
-
+// ----------------------------------------------------------------------------
 server.listen(config.port);
 syslog.log(syslog.LOG_INFO, "notifier started on port " + config.port);
+// ---------------------------------------------------------------------
+// Watchdog
+// ---------------------------------------------------------------------
+setInterval(function() {
+                var now = new Date();
+                var diff = now - last_watch;
+                if (diff > 3600 * 1000) {
+                    send_mail("no watchdog receive", "Since " + diff + " seconds", function(msg) {
+                        // nothing to do
+                    });
+                }
+            },
+            300 * 1000);
+
